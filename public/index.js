64be5f646284c40be0647a8a05059342852e519b
Effeppi <fran.pes93@gmail.com>
2020-12-05 00:56:55 +0100

    align center

--- public/index.js
+++ public/index.js
@@ -7,6 +7,8 @@ $(function () {
     });
     socket.on('chat message', function(msg){
       $('#messages').append($('<li>').text(msg));
-      window.scrollTo(0, document.body.scrollHeight);
+
+      $('#messages').scrollTop($('#messages')[0].scrollHeight);
+
     });
   });
